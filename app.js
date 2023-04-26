13796b900a36805fa7476cc62486a99cc8ca4227
Izaias <106784902+IzaiasDataScientist@users.noreply.github.com>
2023-04-26 13:36:11 -0400

    Update try carch finally app.js

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ const telegram = new Telegram(process.env.CHAT_ID);
 
 const data = await bet365._nameCompetition();
 
-await telegram.send_to_telegram('Jogos que não estão empatados.');
-await telegram.send_to_telegram(bet365._obterDados(data, bet365.jogosNaoEmpatados));
+try {
+    await telegram.send_to_telegram('Jogos que não estão empatados.');
+    await telegram.send_to_telegram(bet365._obterDados(data, bet365.jogosNaoEmpatados));
 
-await telegram.send_to_telegram('Jogos que estão empatados.');
-await telegram.send_to_telegram(bet365._obterDados(data, bet365.jogosEmpatados));
+    await telegram.send_to_telegram('Jogos que estão empatados.');
+    await telegram.send_to_telegram(bet365._obterDados(data, bet365.jogosEmpatados));
 
-await telegram.send_to_telegram('Jogos que tiveram mais de 3 gols.');
-await telegram.send_to_telegram(bet365._obterDados(data, bet365.maisGols));
+    await telegram.send_to_telegram('Jogos que tiveram mais de 3 gols.');
+    await telegram.send_to_telegram(bet365._obterDados(data, bet365.maisGols));
 
-await telegram.send_to_telegram('Jogos que tiveram mais de 2 gols.');
-await telegram.send_to_telegram(bet365._obterDados(data, bet365.maiorDoisGols));
-
-await bet365.browser.close();
+    await telegram.send_to_telegram('Jogos que tiveram mais de 2 gols.');
+    await telegram.send_to_telegram(bet365._obterDados(data, bet365.maiorDoisGols));
+} catch (error) {
+    console.error(`Erro ao enviar mensagem para o Telegram: ${error}`)
+} finally {
+    await bet365.browser.close();
+}
